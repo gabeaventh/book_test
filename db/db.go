eed2214dae0ee134ea8925a17e619265eae10bf9
gabeaventh <gabeaventh@gmail.com>
2024-04-24 03:18:44 +0700

    feat: add env

--- db/db.go
+++ db/db.go
@@ -1,15 +1,22 @@
 package db
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/nedpals/supabase-go"
 )
 
-const (
-	supabaseURL = "https://olfhzqabncwfpyjqnxix.supabase.co"
-	supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sZmh6cWFibmN3ZnB5anFueGl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzOTc3OTksImV4cCI6MjAyODk3Mzc5OX0.LfsNF4e_SPdIDYk8xjbaLx5lFhlpVQ3Rfd75ICH6lqQ"
-)
-
 func InitDB() *supabase.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	supabaseURL := os.Getenv("SUPABASE_URL")
+	supabaseKey := os.Getenv("SUPABASE_KEY")
+
 	client := supabase.CreateClient(supabaseURL, supabaseKey)
 
 	return client
